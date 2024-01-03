c823a4f6bf4911ee33cb177099f809035e207bdb
Vaibhav <sukalevaibhav53@gmail.com>
2024-01-04 00:09:11 +0530

    hooks in react

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 //import App from './App.js';
 
 //properties passed 
-function header(props){
-  return <h1>Hello {props}</h1>
-}
+// function header(props){
+//   return <h1>Hello {props}</h1>
+// }
 //Map function
 // function App(){
 //   const people = ["John", "Dave","Jane"];
@@ -17,13 +17,33 @@ function header(props){
 //   ) 
 // }
 
-function Person(props){
-  function handlePersonClick(event){
-    alert('props.person')
-    console.log(event)
+// function Person(props){
+//   function handlePersonClick(event){
+//     alert('props.person')
+//     console.log(event)
+//   }
+//   return <li onClick ={handlePersonClick}>{props.Person}</li>
+// }
+
+// React hooks
+ function App(){
+  const inputState = React.useState('some text');
+  const inputValue = inputState[0];
+  // function to change to input states
+  const setInputValue = inputState[1];
+  function handleInputChange(event){
+    //value = event.target.value;
+    //console.log(value);
+    setInputValue(event.target.value);
   }
-  return <li onClick ={handlePersonClick}>{props.Person</li>
-}
+  return (
+    <div>
+      <input onChnage ={handleInputChange}/>
+      <p>{inputValue}</p>
+    </div>
+  )
+ }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
