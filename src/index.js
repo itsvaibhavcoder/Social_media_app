c7959c9004225edb8e98f3e0404405d96e268c3e
Vaibhav <sukalevaibhav53@gmail.com>
2024-01-04 16:55:13 +0530

    added new component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import Hooks from './hooks.js'; // Correct import statement: Use PascalCase for components
-import Position from './mousePositionTrack.js';
+// import Position from './mousePositionTrack.js';
+
+import API from './ApiUseEffectHook';
 
 ReactDOM.render(
   <div>
     {/* <Hooks /> */}
-    <Position/>
+    {/* {<Position/>} */}
+    <API/>
   </div>,
   document.getElementById('root')
 );
