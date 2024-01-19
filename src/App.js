f0794c46f685195a922ab071f9523f6159b8ac63
Vaibhav <sukalevaibhav53@gmail.com>
2024-01-20 02:23:07 +0530

    added component in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from "react";
 import Login from './components/Login';
 import Header from "./components/Header";
-function App(){
-    // let user = "";
-    const [user, setUser]= React.useState('vaibhav');
-    // React.useEffect(()=>{
-    //     document.title = user;
-    // }, [user]);
-    React.useEffect(()=>{
-      document.title = user? `${user}'s Feed`: `please Login`;
-    },[user]);
+import CreatePost from "./components/CreatePost";
 
-    if(!user){
-        return <div>
-        <Login setUser={setUser}/>
-    </div>
-    }
-    else{
-        return <>
-        <Header user ={user} setUser={setUser}/>
-        </>
+function App() {
+    const [user, setUser] = React.useState('vaibhav');
+    const [posts, setPosts] = React.useState([]);
+
+    React.useEffect(() => {
+        document.title = user ? `${user}'s Feed` : 'Please Login';
+    }, [user]);
+
+    if (!user) {
+        return (
+            <div>
+                <Login setUser={setUser} />
+            </div>
+        );
+    } else {
+        return (
+            <>
+                <Header user={user} setUser={setUser} />
+                <CreatePost user={user} setPosts={setPosts} posts={posts} />
+                {posts.map((post,i) => (
+                    <div>
+                        {post.image && post.content && (
+                            <React.Fragment key={i}>
+                                <img
+                                    style={{ height: 100, width: 200, objectFit: 'cover' }}
+                                    src={URL.createObjectURL(post.image)}
+                                    alt="Post cover"
+                                />
+                                <p>{post.content}</p>
+                                <div>{user}</div>
+                            </React.Fragment>
+                        )}
+                    </div>
+                ))}
+            </>
+        );
     }
 }
 
