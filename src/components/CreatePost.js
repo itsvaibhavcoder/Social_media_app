c5dc95c7d1f596c6854d0c8e189ecf21e4f3f189
Vaibhav <sukalevaibhav53@gmail.com>
2024-01-20 02:18:38 +0530

    Added

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,8 +1,39 @@
 import React from "react";
 
-function CreatePost(){
+function CreatePost({user,setPosts,posts}){
+    const[content, setContent] = React.useState("");
+    const [image, setImage] = React.useState(null);
+    const imageInputRef = React.useRef();
+    function handleSubmit(event){
+        event.preventDefault();
+        const post = {content:content, image:image, user:user};
+        const newPosts = [post, ...posts]
+        setPosts(newPosts);
+        setContent("");
+        imageInputRef.current.value ='';
+    }
     return (
-        <div>createpost</div>
+        <div>
+           <h1>Create New Post</h1>
+           <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            placeholder="Add Post Content"
+            onChange={event=> setContent(event.target.value)}
+            value={content}/>
+            <input
+            type="file"
+            onChange={event=>setImage(event.target.files[0])}
+            ref ={imageInputRef}/>
+            <button type = "submit">Submit Post</button>
+            </form> 
+
+            {/* <p>{content}</p>
+            {image && <img 
+            style ={{height:100, width: 200, objectFit: "cover"}}
+            src={URL.createObjectURL(image)}
+            />} */}
+        </div>
     );
 }
 
